0373522f234496a1a2dd7a8533b5a0608b6bc325
ravi-devo <ravi.ravichandran370@gmail.com>
2024-06-05 20:10:03 +0530

    Backend URL update

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,5 +1,5 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
-const baseQuery = fetchBaseQuery({ baseUrl: 'http://localhost:8000'});
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://expensetracker-be-d1t3.onrender.com'});
 
 export const apiSlice = createApi({
     baseQuery,
